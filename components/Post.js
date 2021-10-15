db17afbdc6e7097e4248ed624a3de3b8538d7b39
erics1337 <erics1337@gmail.com>
2021-10-14 23:18:50 -0600

    Image and Buttons Sections of Post

--- components/Post.js
+++ components/Post.js
@@ -14,14 +14,26 @@ function Post({ id, username, userImg, img, caption }) {
         <div className="bg-white my-7 border rounded-sm">
             {/* Header */}
             <div className="flex items-center p-5">
-                <img className="rounded-full h-12 w-12 object-contain border p-1 mr-3" src={userImg} alt="" />
-                <p className="flex-1 font-bold">{username}</p>
+                <img className="rounded-full h-12 w-12 object-contain border p-1 mr-3" src={userImg} alt="" 
+                />
+                <p className="flex-1 font-bold">{username}
+                </p>
                 <DotsHorizontalIcon className="h-5" />
             </div>
 
             {/* img */}
+            <img src={img} className="object-cover w-full" alt="" />
 
             {/* Buttons */}
+            <div className='flex justify-between px-4 pt-4'>
+                <div className='flex space-x-4'>
+                    <HeartIcon className="btn" />
+                    <ChatIcon className="btn" />
+                    <PaperAirplaneIcon className="btn" />
+                </div>
+            <BookmarkIcon className="btn"/>
+            </div>
+
 
             {/* caption */}
 
