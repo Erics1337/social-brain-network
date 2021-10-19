321dcf22f3c83ba33259256b16450f4007e3c93a
erics1337 <erics1337@gmail.com>
2021-10-19 17:09:00 -0600

    Display UI Based on Logged In or Not

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -3,16 +3,18 @@ import { signOut, useSession } from "next-auth/react"
 function MiniProfile() {
     const { data: session } = useSession()
 
-    console.log(session)
+    // console.log(session)
 
     return (
         <div className="flex items-center justify-between mt-14 ml-10">
-            <img className="rounded-full border w-16 h-16" src="https://links.papareact.com/3ke" alt="" />
+            <img className="rounded-full border w-16 h-16" src={session?.user?.image} alt="" />
             <div className="mx-4">
-                <h2 className="font-bold">Eric</h2>
+                {/* ? protects code from asynchronous behavior of the signin callback */}
+                <h2 className="font-bold">{session?.user?.username}</h2>
                 <h3 className="text-sm">Welcome to Instagram</h3>
             </div>
-            <button className="text-blue-400 text-sm font-semibold">Sign Out</button>
+            <button onClick={signOut}
+            className="text-blue-400 text-sm font-semibold">Sign Out</button>
         </div>
     )
 }
