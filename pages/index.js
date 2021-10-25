2c85585b9c1908b3988badc7b2ecf2b385e3be4e
Eric <erics1337@gmail.com>
2021-10-25 14:05:19 -0600

    Auth Checking Moved to Index

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,26 @@ import Head from 'next/head'
 import Feed from '../components/Feed'
 import Header from '../components/Header';
 import Modal from '../components/Modal';
+import Homepage from '../components/Homepage';
+import { useSession } from "next-auth/react"
 
 export default function Home() {
+  const { data: session } = useSession()
+
   return (
     <div className="bg-grey-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
-        <title>Instaclone build</title>
+        <title>Social Brain</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header />
+
+      {session ? (
       <Feed />
+      ) : (
+        <Homepage  />
+    )}
 
       {/* Feed */}
       <Modal />
