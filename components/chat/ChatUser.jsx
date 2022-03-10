3314e1c389472c186e30924a46a27768522e3bc8
Eric <erics1337@gmail.com>
2022-03-09 23:02:08 -0700

    Bug Fix: If No Latest Message Text

--- components/chat/ChatUser.jsx
+++ components/chat/ChatUser.jsx
@@ -36,8 +36,8 @@ function ChatUser({ user }) {
 						</Moment>
 					</div>
 					<span className='block ml-2 text-sm text-gray-600'>
-						{user.latestMessage.text.substring(0, 20)}
-						{user.latestMessage.text.length > 20 && '...'}
+						{user.latestMessage.text?.substring(0, 20)}
+						{user.latestMessage.text?.length > 20 && '...'}
 					</span>
 				</div>
 			</a>
